197ab399e17f3daf839031c83969e71b29348a4f
nilsreich <nilsreich@gmail.com>
2022-11-25 07:48:34 +0000

    Update 10 files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
-import { BroadcastChannel } from 'broadcast-channel';
-import { useEffect, useState } from 'react';
-import { Navbar } from '../components/Navbar';
+import { useEffect, useState } from "react";
+import { Navbar } from "../components/Navbar";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkMath from "remark-math";
+import remarkRehype from "remark-rehype";
+import rehypeKatex from "rehype-katex";
+import rehypeStringify from "rehype-stringify";
+
 export default function Home() {
-  const channel = new BroadcastChannel('foobar');
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("a");
   const [coords, setCoords] = useState({ x: 0, y: 0 });
   const [globalCoords, setGlobalCoords] = useState({ x: 0, y: 0 });
+  let bc = null;
+  if (typeof window != "undefined") {
+    bc = new BroadcastChannel("thechannel");
+  }
 
   useEffect(() => {
     // 👇️ get global mouse coordinates
@@ -15,39 +24,62 @@ export default function Home() {
         y: event.screenY,
       });
     };
-    window.addEventListener('mousemove', handleWindowMouseMove);
-
+    window.addEventListener("mousemove", handleWindowMouseMove);
     return () => {
-      window.removeEventListener('mousemove', handleWindowMouseMove);
+      window.removeEventListener("mousemove", handleWindowMouseMove);
     };
   }, []);
 
   useEffect(() => {
-    channel.postMessage({ msg: message, coords: coords });
+    bc.postMessage(JSON.stringify({ msg: message, coords: coords }));
   }, [coords]);
 
   const handleMouseMove = (event) => {
+    console.log(event.target.offsetTop);
     setCoords({
-      x: event.clientX - event.target.offsetLeft,
-      y: event.clientY - event.target.offsetTop,
+      x: event.clientX,
+      y: event.clientY,
     });
   };
 
   const send = (msg) => {
     setMessage(() => msg);
-    channel.postMessage({ msg: msg, coords: coords });
+    const temp = JSON.stringify({ msg: msg, coords: coords });
+    bc.postMessage(temp);
   };
 
   return (
-    <div>
+    <div className="h-screen bg-slate-100">
       <Navbar />
-      <div
-        onMouseMove={handleMouseMove}
-        style={{ width: '100vw', height: '100vh' }}
-      >
-        <textarea value={message} onChange={(e) => send(e.target.value)} />
-        <button onClick={send}>SEND</button>
-        Coords: {coords.x} {coords.y}
+      <div className="flex h-full w-full">
+        <div className="w-1/2  cursor-none	" onMouseMove={handleMouseMove}>
+          <div
+            className="h-4 w-4  bg-opacity-90 blur-sm rounded-full bg-red-500 absolute"
+            style={{
+              left: coords.x,
+              top: coords.y,
+            }}
+          ></div>
+          <div
+            className="prose"
+            dangerouslySetInnerHTML={{
+              __html: String(
+                unified()
+                  .use(remarkParse)
+                  .use(remarkMath)
+                  .use(remarkRehype)
+                  .use(rehypeKatex)
+                  .use(rehypeStringify)
+                  .processSync(message)
+              ),
+            }}
+          ></div>
+        </div>
+        <textarea
+          className="grow h-full"
+          value={message}
+          onChange={(e) => send(e.target.value)}
+        />
       </div>
     </div>
   );
--- pages/screen.tsx
+++ pages/screen.tsx
@@ -1,31 +1,48 @@
-import { BroadcastChannel } from 'broadcast-channel';
-import { useState, useEffect } from 'react';
-import { Navbar } from '../components/Navbar';
+import { useState, useEffect } from "react";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkMath from "remark-math";
+import remarkRehype from "remark-rehype";
+import rehypeKatex from "rehype-katex";
+import rehypeStringify from "rehype-stringify";
 export default function Screen() {
-  const channel = new BroadcastChannel('foobar');
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [pointer, setPointer] = useState({ x: 0, y: 0 });
+  let bc = null;
+  if (typeof window != "undefined") {
+    bc = new BroadcastChannel("thechannel");
+  }
   useEffect(() => {
-    channel.onmessage = (msg) => {
-      setMessage(() => msg.msg);
-      setPointer(() => msg.coords);
+    bc.onmessage = (msg: any) => {
+      let data = JSON.parse(msg.data);
+      setMessage(() => data.msg);
+      setPointer(() => data.coords);
     };
   }, []);
 
   return (
     <div>
-      <Navbar />
-      <div>{message}</div>
       <div
-        style={{
-          position: 'absolute',
-          backgroundColor: 'red',
-          left: pointer.x,
-          top: pointer.y,
+        className="prose"
+        dangerouslySetInnerHTML={{
+          __html: String(
+            unified()
+              .use(remarkParse)
+              .use(remarkMath)
+              .use(remarkRehype)
+              .use(rehypeKatex)
+              .use(rehypeStringify)
+              .processSync(message)
+          ),
         }}
-      >
-        x
-      </div>
+      ></div>
+      <div
+        className="h-4 w-4  bg-opacity-90 blur-sm rounded-full bg-red-500 absolute"
+        style={{
+          left: pointer.x - 24,
+          top: pointer.y - 24,
+        }}
+      ></div>
     </div>
   );
 }
